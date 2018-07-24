a2c940353b2c2c484f643241184c0d1413cca2cf
ImM7MD <40389600+ImM7MD@users.noreply.github.com>
2018-07-24 11:26:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-cconst Discord = require('discord.js');
+const Discord = require('discord.js');
 const A7MD = new Discord.Client();
 let timer;
 console.log("BOT ONLINE");
@@ -7,24 +7,21 @@ A7MD.on("guildMemberAdd", member => {
     timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
   let words = [` 
   **
-**هاذا سرفرى  : 
-سرفر اسطورى اقسم بالله 
-[- الفاعليه القادمه ]
-كيب  OptiFine  -
-رانك  في اي بي هاى بكسل  -
-حسابين فول اكسس ماين كرافت - 
---> شروط الفاعليه <--
-لو انته فزت الكيب لازم تدخل  تكون مدخل 15 عضو
-لو انته فزت ال فى اى بى لازم تكون مدخل 10 اعضاء 
-والحسابين الفول اكسس عادى
----------------------------------------
-الى ما راح ينفذ القوانين ما راح ياخد شي حتى لو فزت 
-وبس 
-تم التغير من النيترو لكيب
-لمعرفة كم شخص جبته للسيرفر اكتب** `!invites`
-**رابط :link: ***https://discord.gg/7g22RFp*** [ السيرفر ] 
-The invite for yuo ... [ ${member}  ]
-**`) 
+  :hearts:️:paperclips:.
+  :heart_eyes: :heart: 
+  لسنا الافضل لاكن بوجودك نستمر حياك ف سيرفر سهرات  :musical_score::sparkles:
+                                 [ https://discord.gg/TeQvr7b ] 
+  الدعووة خاصة لك ... [ ${member}  ]
+  **`,`سلام عليكم ياحلو ,تعال ونورنا ناقصنا تواجدك:candle::black_heart:.
+  رابط:https://discord.gg/TeQvr7b`,` -رُبما صدفة من غير حُسبان 
+  نصبح بها اصدقاء :heartbeat:.
+  
+  LINK : https://discord.gg/TeQvr7b `]
+  setTimeout(() =>{
+  member.createDM().then(function (channel) {
+  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
 }).catch(console.error)
+}, timer)
 })
+
 client.login(process.env.BOT_TOKEN);
